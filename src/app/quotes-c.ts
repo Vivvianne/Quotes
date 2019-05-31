5b835350363635c31d0370c254b9f2160393aa16
Vivvianne Kimani <ndiahav@gmail.com>
2019-05-31 12:51:05 +0300

    defines the quotes, author and description and creates a constructor for them

--- src/app/quotes-c.ts
+++ src/app/quotes-c.ts
@@ -1,2 +1,8 @@
 export class QuotesC {
-}
+    public Quotes: string;
+    public author: string;
+    public showDescription:boolean;
+    constructor(Quotes:string, author:string,public description:string,public completeDate:Date){
+        this.Quotes= Quotes;
+        this.author=author;
+    }
